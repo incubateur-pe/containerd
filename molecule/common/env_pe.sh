8546da901253066e9d42e5b2e8f0ecd33701272e
Fabien Aubert <fabien.aubert@pole-emploi.fr>
2020-11-26 09:30:33 +0100

    refactor: rename environment files

--- molecule/common/env_pe.sh
+++ molecule/common/env_pe.sh
@@ -1,3 +1,9 @@
 DOCKER_YUM_MIRROR=https://download.docker.com/linux/
 DOCKER_APT_MIRROR=https://download.docker.com/linux/
 CONTAINERD_MIRRORS=[{"name":"quay.io","endpoint":["http://10.0.4.40:5002"]},{"name":"test","endpoint":["https://10.0.4.40.5001"],"skip_ssl_verify":true},{"name":"docker.io","endpoint":["http://10.0.4.40:5005"]}]
+http_proxy
+https_proxy
+HTTP_PROXY
+HTTPS_PROXY
+no_proxy
+NO_PROXY
