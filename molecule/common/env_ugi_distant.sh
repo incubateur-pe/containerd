cf381403e0e8fe7900731ef3debe2cc676a8fee7
ulrich giraud <ulrich.giraud1@gmail.com>
2020-11-29 11:38:30 +0100

    refactor(config): move ssl configuration from mirror variable to configs

--- molecule/common/env_ugi_distant.sh
+++ molecule/common/env_ugi_distant.sh
@@ -1,6 +1,5 @@
 DOCKER_YUM_MIRROR=http://10.0.4.40:8081/repository/docker-yum
 DOCKER_APT_MIRROR=http://10.0.4.40:8081/repository/docker-yum
-CONTAINERD_MIRRORS=[{"name":"quay.io","endpoint":["http://10.0.4.40:5002"]},{"name":"test","endpoint":["https://10.0.4.40.5001"],"skip_ssl_verify":true},{"name":"docker.io","endpoint":["http://10.0.4.40:5005"]}]
 REMOTE_HOST=true
 PROVIDER_ARGS=['host="kvm"', 'username="libvirt-user"']
 SSH_CONFIG=ssh_config_ugi
